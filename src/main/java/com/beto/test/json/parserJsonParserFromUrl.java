104ed2e014ca41a596a04f69ea30a56446648a3d
Alexandr Kidyuk <kidyuk@mail.ru>
2016-07-22 15:05:15 +0300

    Json parsing from Url

---
+++ src/main/java/com/beto/test/json/parserJsonParserFromUrl.java
@@ -0,0 +1,61 @@
+package com.beto.test.json;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.util.List;
+
+public class parserJsonParserFromUrl {
+
+    public static Data getData(String url) throws Exception {
+
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet(url);
+        httpGet.addHeader("accept", "application/json");
+        HttpResponse response = httpClient.execute(httpGet);
+        String data =  readData(response);
+
+        Gson gson = new Gson();
+
+        return gson.fromJson(data, Data.class);
+    }
+
+    public static List<Data> getDataList(String url) throws Exception {
+
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet(url);
+        httpGet.addHeader("accept", "application/json");
+        HttpResponse response = httpClient.execute(httpGet);
+        String data =  readData(response);
+
+        Gson gson = new Gson();
+        Type type = new TypeToken<List<Data>>(){}.getType();
+        return gson.fromJson(data, type);
+    }
+
+    public static String readData(HttpResponse response) throws Exception {
+        BufferedReader bufferedReader = null;
+        try {
+            bufferedReader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+            StringBuilder buffer = new StringBuilder();
+            char[] dataLength = new char[1024];
+            int read;
+            while ((read = bufferedReader.read(dataLength)) != -1){
+                buffer.append(dataLength, 0, read);
+            }
+            return buffer.toString();
+        } finally {
+            if (bufferedReader != null){
+                bufferedReader.close();
+            }
+        }
+    }
+}
---
+++ src/test/java/TestJson.java
@@ -0,0 +1,42 @@
+import com.beto.test.json.Data;
+import com.beto.test.json.parserJsonParserFromUrl;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by energetic on 22.07.2016.
+ */
+public class TestJson {
+
+    @Test
+    public void test1() throws Exception {
+        System.out.println("*****************************\n" +
+                "URL: https://jsonplaceholder.typicode.com/posts/1");
+        Data data = parserJsonParserFromUrl.getData("https://jsonplaceholder.typicode.com/posts/1");
+
+        System.out.println("result: \n" + data);
+        System.out.println("*****************************");
+        String s = data.toString();
+        String[] s1 = s.split(",");
+        List<String> s2 = new ArrayList<String>();
+        for (String a : s1){
+            String b = a + "\n";
+            s2.add(b);
+        }
+        System.out.println(s2);
+    }
+    @Test
+    public void testList() throws Exception {
+        System.out.println("*****************************\n" +
+                "URL: https://jsonplaceholder.typicode.com/posts");
+        List<Data> dataList = parserJsonParserFromUrl.getDataList("https://jsonplaceholder.typicode.com/posts");
+
+        System.out.println("result: \n" + dataList);
+        String s = "Hel,lo";
+        String[] s1 = s.split(",");
+        System.out.println(Arrays.toString(s1));
+    }
+}
